ba3c6a5c31cb0fb64756ba86fa8b9d5d8a4a775b
Renato <renatojosesfc@gmail.com>
2025-10-30 13:26:43 -0300

    Add files via upload

---
+++ lista22/src/lista22/conta.java
@@ -0,0 +1,5 @@
+package lista22;
+
+public class conta {
+
+}
---
+++ lista22/src/lista22/contaP.java
@@ -0,0 +1,10 @@
+package lista22;
+
+public class contaP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
